8029a45404ef171d95b32616fe7feea25deb8e59
Ian Currie <iansedano@gmail.com>
2024-09-16 17:07:49 +0200

    Basic styling and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: "Inria Sans, sans-serif",
+        mono: "Courier Prime, monospace",
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
