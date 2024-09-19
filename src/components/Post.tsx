678025d5f258b1c03dd77889f0436d99939e4ae8
Ian Currie <iansedano@gmail.com>
2024-09-19 20:42:43 +0200

    Style and tpye

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,12 @@
-const Post = ({ title, content }) => {
+import { Component } from "solid-js";
+
+const Post: Component<{ title: string; content: string }> = ({
+  title,
+  content,
+}) => {
   return (
     <article>
-      <h1 class="text-3xl font-bold my-8 text-center">{title}</h1>
+      <h1 class="text-4xl font-bold my-8 text-center">{title}</h1>
 
       <div class="prose mx-auto" innerHTML={content}></div>
     </article>
