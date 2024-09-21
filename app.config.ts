5889c9d6aabb8aa9f42ac08c00ea98028e6587f1
Ian Currie <iansedano@gmail.com>
2024-09-21 16:07:47 +0200

    Add vite server restart on change to md

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "@solidjs/start/config";
+import ViteRestart from "vite-plugin-restart";
 
 export default defineConfig({
   server: {
     prerender: {
       crawlLinks: true,
     },
+  },
+  vite: {
+    plugins: [
+      ViteRestart({
+        restart: ["src/md/*.*"],
+      }),
+    ],
   },
 });
