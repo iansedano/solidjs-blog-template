4b5677bb81263fe11ad17849d146698fbbcd8665
Ian Currie <iansedano@gmail.com>
2024-09-22 11:10:37 +0200

    Add routing indicator

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
-import { useLocation } from "@solidjs/router";
+import { useLocation, A, useIsRouting } from "@solidjs/router";
 
 export default function Nav() {
+  const isRouting = useIsRouting();
   const location = useLocation();
   const active = (path: string) =>
     path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
@@ -9,11 +10,13 @@ export default function Nav() {
     <nav class="bg-slate-200">
       <ul class="flex items-center justify-center p-3">
         <li class={`${active("/")} mx-1.5 border-b-2 sm:mx-6`}>
-          <a href="/">Home</a>
+          <A href="/">Home</A>
         </li>
         <li class={`${active("/blog")} mx-1.5 border-b-2 sm:mx-6`}>
-          <a href="/blog">Blog</a>
+          <A href="/blog">Blog</A>
         </li>
+
+        {isRouting() ? <div class="animate-bounce">⏳</div> : ""}
       </ul>
     </nav>
   );
